0b1c8b4dd75048dfc080bdb4e8dba9adc00054f7
Rodrigo Toste Gomes <rgomes@mit.edu>
2013-11-29 02:13:25 -0500

    Updated list of applications to install, added file for old network interface names to install.sh and updated procedure to set settings on gnome.

--- arch/install.sh
+++ arch/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cp 01-mysettings /etc/dconf/db/gdm.d/
+rm /etc/dconf/db/gdm
+dconf update
 cp krb5.conf /etc/
 cp sync/firefox_profile_get /etc/X11/xinit/xinitrc.d/
 cp sync/firefox_profile_save /etc/gdm/PostSession/
@@ -9,3 +11,4 @@ cp idle/python.xmp /usr/share/pixmaps
 cp 6.01Soft/CMax.desktop /usr/share/application
 cp 6.01Soft/soar.desktop /usr/share/application
 cp -R skel /etc/skel
+ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
